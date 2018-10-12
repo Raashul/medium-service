c914c260c89fd391b4b7510c21b7d535da08df1f
sauasmast <sdahal@caldwell.edu>
2018-10-12 09:04:28 -0400

    Up and running for the deployment

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const morgan = require('morgan');
+var fs = require('fs');
+var path = require('path');
+
+const app = express();
+
+// log all the logs into the txt file called access.log
+// write system in append mode. 
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+
+//setting up the logger
+app.use(morgan('combined', { stream: accessLogStream }))
+
+
+app.get('/', (req, res) => {
+  console.log('first get request');
+  res.send('<h2> Hello from server. Welcome to the medium client api');
+})
+
+
+app.listen(3003, () => {
+  console.log('starting app in port 3003');
+})
