262e5c5a484e040ee3e218f5ca5a726ba2cc6b93
sauasmast <sdahal@caldwell.edu>
2018-10-12 21:12:48 -0400

    File with the database connection

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-const express = require('express');
-const morgan = require('morgan');
-var fs = require('fs');
-var path = require('path');
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+var fs = require('fs')
+var path = require('path')
+const mysql = require('mysql')
 
 const app = express();
 
@@ -11,6 +13,26 @@ var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {
 
 //setting up the logger
 app.use(morgan('combined', { stream: accessLogStream }))
+
+app.get('/user/:id', (req, res) => {
+  
+  const connection = mysql.createConnection({
+    host: process.env.db_host,
+    user: process.env.db_user,
+    password: process.env.db_password,
+    database: process.env.db_name,
+    port:process.env.db_port,
+    socketPath: process.env.db_sock
+  })
+
+  connection.query("SELECT * FROM users", (err, rows, fields)=>{
+    if (err) {
+      console.log(`Got an error of ${err}`)
+    }
+    console.log("Fetched the user sucessfully.")
+    res.json(rows)
+  })
+})
 
 
 app.get('/', (req, res) => {
