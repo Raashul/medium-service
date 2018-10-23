14c5f030e6aed26d7df892fbd29d682a53940217
Raashul <rashul1996@gmail.com>
2018-10-22 23:58:19 -0400

    add google login

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ require('dotenv').config()
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser')
+const  passport = require('passport');
+const auth = require(__base + '/app/init/auth');
 
 const app = express();
 
@@ -18,6 +20,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(request_id);
 
+auth(passport);
+app.use(passport.initialize());
 
 //initialize routes
 require(__base + '/app/routes/index')(app);
@@ -35,6 +39,21 @@ app.get('/', (req, res) => {
 //   const test  = {a: "good job"}
 //   res.json(test);
 // })
+
+
+app.get('/auth/google', passport.authenticate('google', {
+  scope: ['https://www.googleapis.com/auth/userinfo.profile']
+}));
+app.get('/auth/google/callback',
+  passport.authenticate('google', {
+      failureRedirect: '/'
+  }),
+  (req, res) => {
+    console.log('sucess');
+    res.send('Success login');
+    
+  }
+);
 
 // start listening to port
 const server = app.listen(config.app.port, () => {
